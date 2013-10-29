dea60261c454f22bfb407bedaa02663b3d0d07b3
Rafael Franco <rafaelfranco@me.com>
2013-10-29 10:23:57 -0200

    kidu.com.br

--- app/config/envinroments.php
+++ app/config/envinroments.php
@@ -7,7 +7,7 @@
     #define constants for envinronments
     define('DEVEVOPMENT_URL','local.kidu.com.br');
     define('TEST_URL','kidu.rfti.com.br');
-    define('PRODUCTION_URL','uspro1056.startdedicated.com');
+    define('PRODUCTION_URL','www.kidu.com.br');
 
 
 ?>
