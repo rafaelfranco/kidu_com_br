734d25eaf31addd22dcfb9f8c387ceeeb1e43da8
Rafael Franco <rafaelfranco@me.com>
2013-11-05 12:27:48 -0200

    Likes

--- app/control/profile.php
+++ app/control/profile.php
@@ -74,7 +74,7 @@ class profile extends simplePHP {
                     $answers_html .= '<figure>
                                         <img onclick="showModal('.$answer->guid.')" src="'.$answer->file_icon.'" height="285" width="285" alt="Kidu">
                                         <figcaption>
-                                            <span><img src="/images/ico_curtir.gif" class="likeButton" width="36" height="36">'.$answer->likes.'</span>
+                                            <span  onclick="likeItem('.$answer->guid.');" ><img src="/images/ico_curtir.gif" class="likeButton" width="36" height="36"><span id="likes-'.$answer->guid.'" >'.$answer->likes.'</span></span>
                                             <img src="/images/ico_usuario.gif" width="36" height="36" alt="User">   
                                             <strong>'.$answer->owner->name.'</strong>
                                         </figcaption>
