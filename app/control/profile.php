d9f5f20b4660203aafb4e956b5d9e661bc52e61e
Rafael Franco <rafaelfranco@me.com>
2013-11-07 14:03:32 -0200

    loggeduser

--- app/control/profile.php
+++ app/control/profile.php
@@ -32,7 +32,7 @@ class profile extends simplePHP {
               $this->redirect('/');
             } else {
                 //user data
-                $this->keys['name_user'] = $_SESSION['username'];
+                $this->keys['loggeduser'] = $_SESSION['username'];
                 $this->keys['avatar'] = $_SESSION['avatar_url'];
             }
 
