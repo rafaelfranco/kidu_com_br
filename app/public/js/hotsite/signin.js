527085389adb021a7904e8e0260e66abe5fd70b4
Rodolpho Bertolini Junior <rbertolini@reglare.com.br>
2014-02-17 16:15:54 -0300

    melhoria no login e teste de vídeo

--- app/public/js/hotsite/signin.js
+++ app/public/js/hotsite/signin.js
@@ -15,6 +15,7 @@ errors += validateEmpty('password');
 	$('#sendSignin').html('Aguarde');
 	$('#sendSignin').css('background','silver');
 	$('#sendSignin').css('cursor','auto');
+	$('#sendSignin').css('margin','30px 230px 10px');
 		$.ajax({
 			url: '/action/signin/',
 			type: 'POST',
@@ -28,6 +29,10 @@ errors += validateEmpty('password');
 		        	if(dados[0] == 'success') {
 		        		window.location = '/profile';
 		        	} else {
+			        	$('#sendSignin').html('Entrar');
+						$('#sendSignin').css('background','url("../images/fundo_botoes.jpg") repeat-x scroll 0px 0px transparent');
+						$('#sendSignin').css('cursor','pointer');
+						$('#sendSignin').css('margin','30px 245px 10px');
 						alert('Houve um erro: ' + dados[0] + " " + dados[1]);
 		        	}
 		        }
