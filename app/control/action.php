001ecaab927870e5de86d36941df6a2235025cdf
Rodolpho Bertolini Junior <rbertolini@reglare.com.br>
2014-02-24 23:36:35 -0300

    tentativa de vídeo...

--- app/control/action.php
+++ app/control/action.php
@@ -150,7 +150,7 @@ class action extends simplePHP {
     //save file on ELGG
     //$res = $this->core->callWs('file.upload',array('filepath'=>$link,'container_guid'=>$_POST['challenge_id'],'user_guid'=>$_SESSION['guid'],'access'=>2,'description'=>$youtubeCode));
 
-    echo $youtubeCode;
+    var_dump($youtubeCode);
     //redirect to profile
     // if($res->status == 0) {
     //   $this->redirect('/profile');
--- app/control/core.php
+++ app/control/core.php
@@ -134,142 +134,140 @@ class core extends simplePHP {
       return $doneChallenges;
     } 
 
-public function youtubePost($file,$challenge) {
-// Call set_include_path() as needed to point to your client library.
-require_once 'Google/Client.php';
-require_once 'Google/Service/YouTube.php';
-//session_start();
+// public function youtubePost($file,$challenge) {
+// // Call set_include_path() as needed to point to your client library.
+// require_once 'Google/Client.php';
+// require_once 'Google/Service/YouTube.php';
+// //session_start();
 
-/*
- * You can acquire an OAuth 2.0 client ID and client secret from the
- * Google Developers Console <https://cloud.google.com/console>
- * For more information about using OAuth 2.0 to access Google APIs, please see:
- * <https://developers.google.com/youtube/v3/guides/authentication>
- * Please ensure that you have enabled the YouTube Data API for your project.
- */
-$OAUTH2_CLIENT_ID = '505349315848.apps.googleusercontent.com';
-//$OAUTH2_CLIENT_SECRET = 'kiduthefuture7';
-$OAUTH2_CLIENT_SECRET = 'c41e878479a39921f068fff88a2d76758de2314a';
+// /*
+//  * You can acquire an OAuth 2.0 client ID and client secret from the
+//  * Google Developers Console <https://cloud.google.com/console>
+//  * For more information about using OAuth 2.0 to access Google APIs, please see:
+//  * <https://developers.google.com/youtube/v3/guides/authentication>
+//  * Please ensure that you have enabled the YouTube Data API for your project.
+//  */
+// $OAUTH2_CLIENT_ID = '505349315848-5u3juitck911hip71mci4riosfu5grut.apps.googleusercontent.com';
+// $OAUTH2_CLIENT_SECRET = '5XKg6yisP8VbDhWY-k42XSPb';
 
-$client = new Google_Client();
-$client->setClientId($OAUTH2_CLIENT_ID);
-$client->setClientSecret($OAUTH2_CLIENT_SECRET);
-$client->setScopes('https://www.googleapis.com/auth/youtube');
-$redirect = filter_var('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'],
-    FILTER_SANITIZE_URL);
-$client->setRedirectUri($redirect);
+// $client = new Google_Client();
+// $client->setClientId($OAUTH2_CLIENT_ID);
+// $client->setClientSecret($OAUTH2_CLIENT_SECRET);
+// $client->setScopes('https://www.googleapis.com/auth/youtube');
+// $redirect = filter_var('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'],
+//     FILTER_SANITIZE_URL);
+// $client->setRedirectUri($redirect);
 
-// Define an object that will be used to make all API requests.
-$youtube = new Google_Service_YouTube($client);
+// // Define an object that will be used to make all API requests.
+// $youtube = new Google_Service_YouTube($client);
 
-  if (isset($_GET['code'])) {
-    if (strval($_SESSION['state']) !== strval($_GET['state'])) {
-      die('The session state did not match.');
-    }
+//   if (isset($_GET['code'])) {
+//     if (strval($_SESSION['state']) !== strval($_GET['state'])) {
+//       die('The session state did not match.');
+//     }
 
-    $client->authenticate($_GET['code']);
-    $_SESSION['token'] = $client->getAccessToken();
-    header('Location: ' . $redirect);
-  }
+//     $client->authenticate($_GET['code']);
+//     $_SESSION['token'] = $client->getAccessToken();
+//     header('Location: ' . $redirect);
+//   }
 
-  if (isset($_SESSION['token'])) {
-    $client->setAccessToken($_SESSION['token']);
-  }
+//   if (isset($_SESSION['token'])) {
+//     $client->setAccessToken($_SESSION['token']);
+//   }
 
-// Check to ensure that the access token was successfully acquired.
-  if ($client->getAccessToken()) {
-    try{
-      // REPLACE this value with the path to the file you are uploading.
-      $videoPath = "tmp/".$file;
+// // Check to ensure that the access token was successfully acquired.
+//   if ($client->getAccessToken()) {
+//     try{
+//       // REPLACE this value with the path to the file you are uploading.
+//       $videoPath = "tmp/".$file;
 
-      // Create a snippet with title, description, tags and category ID
-      // Create an asset resource and set its snippet metadata and type.
-      // This example sets the video's title, description, keyword tags, and
-      // video category.
-      $snippet = new Google_Service_YouTube_VideoSnippet();
-      $snippet->setTitle('Resposta de '.$_SESSION['name'].' ao desafio '.$challenge);
-      $snippet->setDescription('Para maiores informações acesse www.kidu.com.br');
-      //$snippet->setTags(array("tag1", "tag2"));
+//       // Create a snippet with title, description, tags and category ID
+//       // Create an asset resource and set its snippet metadata and type.
+//       // This example sets the video's title, description, keyword tags, and
+//       // video category.
+//       $snippet = new Google_Service_YouTube_VideoSnippet();
+//       $snippet->setTitle('Resposta de '.$_SESSION['name'].' ao desafio '.$challenge);
+//       $snippet->setDescription('Para maiores informações acesse www.kidu.com.br');
+//       //$snippet->setTags(array("tag1", "tag2"));
 
-      // Numeric video category. See
-      // https://developers.google.com/youtube/v3/docs/videoCategories/list 
-      $snippet->setCategoryId("22");
+//       // Numeric video category. See
+//       // https://developers.google.com/youtube/v3/docs/videoCategories/list 
+//       $snippet->setCategoryId("22");
 
-      // Set the video's status to "public". Valid statuses are "public",
-      // "private" and "unlisted".
-      $status = new Google_Service_YouTube_VideoStatus();
-      $status->privacyStatus = "unlisted";
+//       // Set the video's status to "public". Valid statuses are "public",
+//       // "private" and "unlisted".
+//       $status = new Google_Service_YouTube_VideoStatus();
+//       $status->privacyStatus = "unlisted";
 
-      // Associate the snippet and status objects with a new video resource.
-      $video = new Google_Service_YouTube_Video();
-      $video->setSnippet($snippet);
-      $video->setStatus($status);
+//       // Associate the snippet and status objects with a new video resource.
+//       $video = new Google_Service_YouTube_Video();
+//       $video->setSnippet($snippet);
+//       $video->setStatus($status);
 
-      // Specify the size of each chunk of data, in bytes. Set a higher value for
-      // reliable connection as fewer chunks lead to faster uploads. Set a lower
-      // value for better recovery on less reliable connections.
-      $chunkSizeBytes = 1 * 1024 * 1024;
+//       // Specify the size of each chunk of data, in bytes. Set a higher value for
+//       // reliable connection as fewer chunks lead to faster uploads. Set a lower
+//       // value for better recovery on less reliable connections.
+//       $chunkSizeBytes = 1 * 1024 * 1024;
 
-      // Setting the defer flag to true tells the client to return a request which can be called
-      // with ->execute(); instead of making the API call immediately.
-      $client->setDefer(true);
+//       // Setting the defer flag to true tells the client to return a request which can be called
+//       // with ->execute(); instead of making the API call immediately.
+//       $client->setDefer(true);
 
-      // Create a request for the API's videos.insert method to create and upload the video.
-      $insertRequest = $youtube->videos->insert("status,snippet", $video);
+//       // Create a request for the API's videos.insert method to create and upload the video.
+//       $insertRequest = $youtube->videos->insert("status,snippet", $video);
 
-      // Create a MediaFileUpload object for resumable uploads.
-      $media = new Google_Http_MediaFileUpload(
-          $client,
-          $insertRequest,
-          'video/*',
-          null,
-          true,
-          $chunkSizeBytes
-      );
-      $media->setFileSize(filesize($videoPath));
+//       // Create a MediaFileUpload object for resumable uploads.
+//       $media = new Google_Http_MediaFileUpload(
+//           $client,
+//           $insertRequest,
+//           'video/*',
+//           null,
+//           true,
+//           $chunkSizeBytes
+//       );
+//       $media->setFileSize(filesize($videoPath));
 
-      // Read the media file and upload it chunk by chunk.
-      $status = false;
-      $handle = fopen($videoPath, "rb");
-      while (!$status && !feof($handle)) {
-        $chunk = fread($handle, $chunkSizeBytes);
-        $status = $media->nextChunk($chunk);
-      }
+//       // Read the media file and upload it chunk by chunk.
+//       $status = false;
+//       $handle = fopen($videoPath, "rb");
+//       while (!$status && !feof($handle)) {
+//         $chunk = fread($handle, $chunkSizeBytes);
+//         $status = $media->nextChunk($chunk);
+//       }
 
-      fclose($handle);
+//       fclose($handle);
 
-      // If you want to make other calls after the file upload, set setDefer back to false
-      $client->setDefer(false);
+//       // If you want to make other calls after the file upload, set setDefer back to false
+//       $client->setDefer(false);
 
-      $htmlBody .= "<h3>Video Uploaded</h3><ul>";
-      $htmlBody .= sprintf('<li>%s (%s)</li>',
-          $status['snippet']['title'],
-          $status['id']);
+//       $htmlBody .= "<h3>Video Uploaded</h3><ul>";
+//       $htmlBody .= sprintf('<li>%s (%s)</li>',
+//           $status['snippet']['title'],
+//           $status['id']);
 
-      $htmlBody .= '</ul>';
+//       $htmlBody .= '</ul>';
 
-    } catch (Google_ServiceException $e) {
-      $htmlBody .= sprintf('<p>A service error occurred: <code>%s</code></p>',
-          htmlspecialchars($e->getMessage()));
-    } catch (Google_Exception $e) {
-      $htmlBody .= sprintf('<p>An client error occurred: <code>%s</code></p>',
-          htmlspecialchars($e->getMessage()));
-    }
+//     } catch (Google_ServiceException $e) {
+//       $htmlBody .= sprintf('<p>A service error occurred: <code>%s</code></p>',
+//           htmlspecialchars($e->getMessage()));
+//     } catch (Google_Exception $e) {
+//       $htmlBody .= sprintf('<p>An client error occurred: <code>%s</code></p>',
+//           htmlspecialchars($e->getMessage()));
+//     }
 
-    $_SESSION['token'] = $client->getAccessToken();
-  } else {
-    // If the user hasn't authorized the app, initiate the OAuth flow
-    $state = mt_rand();
-    $client->setState($state);
-    $_SESSION['state'] = $state;
+//     $_SESSION['token'] = $client->getAccessToken();
+//   } else {
+//     // If the user hasn't authorized the app, initiate the OAuth flow
+//     $state = mt_rand();
+//     $client->setState($state);
+//     $_SESSION['state'] = $state;
 
-    $authUrl = $client->createAuthUrl();
-    $htmlBody = "<p>You need to <a href='" . $authUrl . "'>authorize access</a> before proceeding.<p>";
-  }
-return $htmlBody;
-}
+//     $authUrl = $client->createAuthUrl();
+//     $htmlBody = "<p>You need to <a href='" . $authUrl . "'>authorize access</a> before proceeding.<p>";
+//   }
+// return $htmlBody;
+// }
 
-/*
     public function youtubePost($file,$challenge) {
       
       include 'Zend/Gdata/YouTube.php';
@@ -293,7 +291,6 @@ return $htmlBody;
 
       $yt = new Zend_Gdata_YouTube($httpClient, $applicationId, $clientId, $developerKey);
 
-
       // create a new VideoEntry object
       $myVideoEntry = new Zend_Gdata_YouTube_VideoEntry();
 
@@ -316,7 +313,11 @@ return $htmlBody;
       // Set keywords. Please note that this must be a comma-separated string
       // and that individual keywords cannot contain whitespace
       $myVideoEntry->SetVideoTags('kidu, desafio');
-      $myVideoEntry->SetVideoUnlisted();
+
+      // abaixo, pego em http://stackoverflow.com/questions/11188557/is-it-possible-to-upload-to-youtube-with-the-zend-library-but-delay-listing-it  
+      $unlisted = new Zend_Gdata_App_Extension_Element( 'yt:accessControl', 'yt','http://gdata.youtube.com/schemas/2007', '' );
+      $unlisted->setExtensionAttributes(array(array('namespaceUri' => '', 'name' => 'action', 'value' => 'list'), array('namespaceUri' => '', 'name' => 'permission', 'value' => 'denied')));
+      $myVideoEntry->setExtensionElements(array($unlisted));
 
       // set some developer tags -- this is optional
       // (see Searching by Developer Tags for more details)
@@ -344,7 +345,7 @@ return $htmlBody;
         return $e->getMessage();
       }
     }
-*/
+
     public function likeIcon($guid,$likes,$iliked){//a mudança de funções quando clica acontece via javascript
       if($likes == 0) {
         $html = '<span class="curtir curtir_item_' . $guid . '" onclick="likeItem('.$guid.');" ><img src="/images/ico_curtir_cz.gif" class="likeButton" width="36" height="36"><b class="cinza">'.$likes.'</b></span>';
